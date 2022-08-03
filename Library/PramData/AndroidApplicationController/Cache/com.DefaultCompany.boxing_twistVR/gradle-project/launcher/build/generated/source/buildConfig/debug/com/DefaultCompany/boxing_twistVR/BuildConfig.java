7ee132a92dde60d5b0e12188c480b9eb76a5e82f
lomeli-noe <neolomeli@gmail.com>
2022-08-02 17:55:47 -0700

    Added boxing gloves prefab to controllers

---
+++ Library/PramData/AndroidApplicationController/Cache/com.DefaultCompany.boxing_twistVR/gradle-project/launcher/build/generated/source/buildConfig/debug/com/DefaultCompany/boxing_twistVR/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.DefaultCompany.boxing_twistVR;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.DefaultCompany.boxing_twistVR";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
