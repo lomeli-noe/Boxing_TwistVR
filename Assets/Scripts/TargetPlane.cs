4b7ee3e976e02d91f1985fd4a940c4cf7f5dfcf6
lomeli-noe <neolomeli@gmail.com>
2022-08-04 10:56:22 -0700

    Spawn targets randomly

---
+++ Assets/Scripts/TargetPlane.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPlane : MonoBehaviour
+{
+
+    public Target[] targets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targets = FindObjectsOfType<Target>();
+        for(int i = 0; i < targets.Length; i++)
+        {
+            int num = UnityEngine.Random.Range(0, 2);
+            targets[i].gameObject.SetActive(Convert.ToBoolean(num));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
